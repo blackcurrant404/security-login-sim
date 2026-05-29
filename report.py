f00df52681b06e75309d8ad25750130ec0bccb1b
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-05-29 14:47:34 +0300

    add auth result reporting module with logging and output

--- report.py
+++ report.py
@@ -1,2 +1,8 @@
 def print_report(ip, result):
-    pass
+    with open("auth.log", "a") as new_file:
+        if result == "Accepted":
+            new_file.write("Accepted password for root from " + ip + "\n")
+            print("Authentication succesfull, correct password")
+        else:
+            new_file.write("Failed password for root from " + ip + "\n")
+            print("Authentication failed, wrong password")
