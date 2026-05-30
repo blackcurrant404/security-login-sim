cca4624cff430f6b4abe45cbfab543e02b8e0b23
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-05-30 11:55:35 +0300

    add timestamps from login attempt logs

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from verification import verify
 from report import print_report
+from datetime import datetime
 
 def main():
-    ip, attempt_pass = user_input()
+    ip, attempt_pass, timestamp = user_input()
     result = verify(attempt_pass) 
-    print_report(ip, result)
+    print_report(ip, result, timestamp)
 
 def user_input():
     ip = input("Enter yuor ip (if empty, using default)")
     if ip == "":
         ip = "10.0.0.100"
     attempt_pass = input ("Enter the password:")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    return ip, attempt_pass
+    return ip, attempt_pass, timestamp
 
 if __name__ == "__main__":
     main()
--- report.py
+++ report.py
@@ -1,8 +1,8 @@
-def print_report(ip, result):
+def print_report(ip: str, result: bool, timestamp: str):
     with open("auth.log", "a") as new_file:
         if result:
-            new_file.write("Accepted password for root from " + ip + "\n")
+            new_file.write(timestamp + " Accepted password for root from " + ip + "\n")
             print("Authentication succesfull, correct password")
         else:
-            new_file.write("Failed password for root from " + ip + "\n")
+            new_file.write(timestamp + " Failed password for root from " + ip + "\n")
             print("Authentication failed, wrong password")
