a32320e4ff061ddd3a2c1a9364d5acb9ba3bf0d4
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-05-30 23:13:14 +0300

    clean up code and fix minor issues

--- verification.py
+++ verification.py
@@ -1,11 +1,11 @@
-users = {"root": "bluwhale", "anna": "1234", "mike": "secret5"}
+users = {"root": "bluewhale", "anna": "1234", "mike": "secret5"}
 
 def verify(input_info):
     
     input_username = input_info["username"]
     input_password = input_info["password"]
 
-    if input_username in input_info:
+    if input_username in users:
         return input_password == users[input_username]
 
     return False
