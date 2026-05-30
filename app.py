0b98c2ade788f9fdf8f8682f1ad22f4acab3403d
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-05-30 22:57:20 +0300

    update verification module to support multiple users

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from verification import verify
-from report import print_report
 from datetime import datetime
 
 def main():
-    ip, attempt_pass, timestamp = user_input()
-    result = verify(attempt_pass) 
-    print_report(ip, result, timestamp)
+    input_info = user_input()
+    result = verify(input_info) 
+    ## print_report()
 
 def user_input():
     input_info = {}
@@ -14,8 +13,8 @@ def user_input():
     if ip == "":
         ip = "10.0.0.100"
     input_info["ip"] = ip
-    input_info["username"] = input ("Enter the username: ")
-    input_info["password"] = input ("Enter the password: ")
+    input_info["username"] = input("Enter the username: ")
+    input_info["password"] = input("Enter the password: ")
     input_info["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     return input_info
--- verification.py
+++ verification.py
@@ -1,7 +1,12 @@
 users = {"root": "bluwhale", "anna": "1234", "mike": "secret5"}
 
-PASSWORD = "bluewhale"
-
-def verify(attempt_pass):
+def verify(input_info):
     
-    return attempt_pass == PASSWORD
+    input_username = input_info["username"]
+    input_password = input_info["password"]
+
+    if input_username in input_info:
+        password = users[input_username]
+        return input_password == users[input_username]
+
+    return False
