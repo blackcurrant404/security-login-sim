285b23a74f621c979ab8e6155fab198495e4feb4
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-05-31 20:09:17 +0300

    add login endpoint with POST handling and data forwarding to verify function

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from datetime import datetime
+from verification import verify
 
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def home():
     return "Hello world"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        ip = request.remote_addr
+
+        input_info = {}
+
+        input_info["ip"] = ip
+        input_info["username"] = username
+        input_info["password"] = password
+        input_info["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        verify(input_info)
+    else:
+        return jsonify({"message": "send post request"})
+
+    return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     app.run()
