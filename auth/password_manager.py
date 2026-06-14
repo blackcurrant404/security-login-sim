fc710a543196496430b22b285959e0e73e45aaf3
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-06-14 19:50:08 +0300

    refactor: move username authentication back to verification module

--- auth/password_manager.py
+++ auth/password_manager.py
@@ -1,12 +1,4 @@
 from bcrypt import checkpw
 
-def verify_password(users: dict, input_info: dict):
-    input_username = input_info["username"]
-    input_password = input_info["password"]   
-    
-    if input_username in users.keys():
-        stored_hash = users[input_username]["hash_password"]
-        return checkpw(input_password.encode(), stored_hash.encode())
-
-    return False
-
+def verify_password(stored_hash, input_password: str):
+    return checkpw(input_password.encode(), stored_hash.encode())
--- auth/verification.py
+++ auth/verification.py
@@ -2,8 +2,15 @@ from auth.user_store import load_users
 from auth.password_manager import verify_password
 
 def verify(input_info):
-    
-    users = load_users()
-    result = verify_password(users, input_info)
+
+    users = load_users()   
+    input_username = input_info["username"]
+    input_password = input_info["password"]   
+
+    if input_username in users: 
+        stored_hash = users[input_username]["password_hash"]
+        result = verify_password(stored_hash, input_password)
+    else:
+        result = False
 
     return result
