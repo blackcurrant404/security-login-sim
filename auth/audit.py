9f189acabcd2678c46080db6031b6a06860a245a
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-07-06 13:25:51 +0300

    feat: update audit logging to support new authentication logic

--- auth/audit.py
+++ auth/audit.py
@@ -1,10 +1,16 @@
 def print_report(input_info: dict):
     with open("logs/auth.log", "a") as new_file:
-        if input_info["result"]:
-            new_file.write(f"{input_info['timestamp']} Accepted password for "
+
+        if input_info["result"]["locked"]:
+            new_file.write(f"{input_info['timestamp']} Failed attempt, reason: locked user for "
             f"{input_info['username']} from {input_info['ip']}\n")
-            print("Authentication successful, correct password")
+            print("Failed attempt, reason: locked user")
         else:
-            new_file.write(f"{input_info['timestamp']} Failed password for "
-            f"{input_info['username']} from {input_info['ip']}\n")
-            print("Authentication failed, wrong password or username")
+            if input_info["result"]["verification"]:
+                new_file.write(f"{input_info['timestamp']} Accepted password for "
+                f"{input_info['username']} from {input_info['ip']}\n")
+                print("Authentication successful, correct password")
+            else:
+                new_file.write(f"{input_info['timestamp']} Failed password for "
+                f"{input_info['username']} from {input_info['ip']}\n")
+                print("Authentication failed, wrong password or username")
